109d7408e10a6c83748688a315270c0382808226
Arsam <arsam4waffels@gmail.com>
2026-09-16 19:14:43 +0330

    docs

--- src/main/java/com/javacs/threads/JThreads.java
+++ src/main/java/com/javacs/threads/JThreads.java
@@ -5,6 +5,8 @@ public class JThreads {
      * parallelization  : Executing multiple threads in parallel.
      * concurrency      : Tasks are not performed simultaneously; the CPU
      *                    simply switches between threads very rapidly.
+     * multitasking     : operating system manages multiple programs or processes.
+     * multithreading   : There are several execution paths within a program.
      * [Race Condition]     -> If different threads modify a single piece of data, there is a possibility of error.
      * [synchronized]       -> Queueing system.
      *                      -> The guarantee is simply that the two threads do not enter simultaneously.
@@ -16,6 +18,36 @@ public class JThreads {
      *      [2] utilization of multiple cores
      *      [3] improved application responsiveness
      *      [4] fair resource allocation
+     *
+     * == Thread State ==
+     *      [1] NEW             => built, but not yet executed
+     *      [2] RUNNABLE        => threads start != executing on cpu rn
+     *      [3] BLOCKED         => locked by 'synchronized'; wating for 'monitor-lock'
+     *      [4] WAITING         => wating for smthing to happen
+     *      [5] TIMED_WAITING   => Thread.sleep(1000);
+     *      [6] TERMINATED      => finished executing
+     *
+     * [NOTE]   : you CANNOT re-start a thread => [IllegalThreadStateException]
+     *
+     *               start()
+     *                  │
+     *                  ▼
+     *                 NEW
+     *                  │
+     *                  ▼
+     *               RUNNABLE
+     *        ┌─────────┴─────────┐
+     *        ▼         ▼         ▼
+     *     BLOCKED   WAITING  TIMED_WAITING
+     *        |         |         |
+     *        └─────────┴─────────┘
+     *                  │
+     *                  ▼
+     *               RUNNABLE
+     *                  │
+     *                  │ run() ends
+     *                  ▼
+     *              TERMINATED
      */
 
     public void runDownloadThread() {
@@ -24,4 +56,26 @@ public class JThreads {
         });
         downloadThread.start();
     }
+
+    /*
+     *              JVM
+     *               │ <- thread.start()
+     *        ┌──────┴──────┐
+     *        │             │
+     *     Thread A      Thread B
+     *        │             │
+     *     println()     println()
+     *
+     * Thread A --> ?
+     * Thread B --> ?
+     *
+     * JVM
+     *  │
+     *  │ <- thread.run()
+     *  │
+     *  └── Thread A --> Thread B
+     */
+
+    // volatile : It can help make the changes visible
+    private volatile int progress;
 }
