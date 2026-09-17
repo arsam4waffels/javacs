afa48d78128e0b6ef3d988df93751d0cdd8fe51a
Arsam <arsam4waffels@gmail.com>
2026-09-17 16:34:52 +0330

    practice: ExecutorService in threads

--- src/main/java/com/javacs/threads/JThreads.java
+++ src/main/java/com/javacs/threads/JThreads.java
@@ -1,7 +1,9 @@
 package com.javacs.threads;
 
-import java.io.ObjectStreamException;
 import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class JThreads {
@@ -325,6 +327,18 @@ public class JThreads {
 
             waiterThread.start();
             chiefThread.start();
+        }
+    }
+
+    public void executorServiceThread() {
+        try (ExecutorService executor =
+                     Executors.newFixedThreadPool(3)
+        ) {
+            // Future<?> video    = executor.submit(() -> downloadVideo());
+            // Future<?> audio    = executor.submit(() -> downloadAudio());
+            // Future<?> subtitle = executor.submit(() -> downloadSubtitle());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
