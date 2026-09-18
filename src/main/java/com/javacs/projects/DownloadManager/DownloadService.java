11b793fefac436965aaa5a51805c7fae45cec47a
Arsam <arsam4waffels@gmail.com>
2026-09-18 13:13:33 +0330

    practice: project/ initialize DownloadService

---
+++ src/main/java/com/javacs/projects/DownloadManager/DownloadService.java
@@ -0,0 +1,9 @@
+package com.javacs.projects.DownloadManager;
+
+public class DownloadService {
+
+    public static void main(String[] args) {
+        DownloadManager downloadManager = new DownloadManager();
+        downloadManager.runnable();
+    }
+}
