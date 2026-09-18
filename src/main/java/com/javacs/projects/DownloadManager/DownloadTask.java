9f8b86a396a1b6da8db3e65aed0aa8e01c7749aa
Arsam <arsam4waffels@gmail.com>
2026-09-18 12:55:09 +0330

    practice: project/ initialize DownloadTask

--- src/main/java/com/javacs/projects/DownloadManager/DownloadTask.java
+++ src/main/java/com/javacs/projects/DownloadManager/DownloadTask.java
@@ -1,4 +1,50 @@
 package com.javacs.projects.DownloadManager;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class DownloadTask {
+
+    private final String fileName;
+    private volatile boolean isDownloadComplete = false;
+
+    private final AtomicInteger downloadPercentage =
+            new AtomicInteger(0);
+
+    public DownloadTask(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void download() {
+
+        Thread downloadThread = new Thread(() -> {
+
+            while (!isDownloadComplete) {
+                System.out.println(
+                        fileName
+                                + ": %"
+                                + downloadPercentage.getAndIncrement()
+                );
+
+                try {
+                    Thread.sleep(300);
+                }
+                catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+
+                if (downloadPercentage.get() >= 100) {
+                    isDownloadComplete = true;
+                    return;
+                }
+            }
+            System.out.println("[Download Completed]");
+        });
+        downloadThread.start();
+        try {
+            downloadThread.join();
+        }
+        catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 }
