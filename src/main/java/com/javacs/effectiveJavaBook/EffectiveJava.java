eedda80bd35498d94c2cba380ca70fa734f804f7
Arsam <arsam4waffels@gmail.com>
2026-09-21 22:05:23 +0330

    practice: class hierarchy

--- src/main/java/com/javacs/effectiveJavaBook/EffectiveJava.java
+++ src/main/java/com/javacs/effectiveJavaBook/EffectiveJava.java
@@ -1,5 +1,9 @@
 package com.javacs.effectiveJavaBook;
 
+import java.util.EnumSet;
+import java.util.Objects;
+import java.util.Set;
+
 public class EffectiveJava {
 
     // Flyweight Pattern
@@ -97,6 +101,29 @@ public class EffectiveJava {
             public NutritionFood build() {
                 return new NutritionFood(this);
             }
+        }
+    }
+
+    @SuppressWarnings("all") abstract class Pizza {
+        public enum Topping { HAM, MUSHROOM, ONION, PEPPER, SAUSAGE }
+        private final Set<Topping> toppings;
+
+        abstract static class Builder<T extends Builder<T>> {
+
+            EnumSet<Topping> toppings = EnumSet.noneOf(Topping.class);
+
+            public T addTopping(Topping topping) {
+                toppings.add(Objects.requireNonNull(topping));
+                return self();
+            }
+
+            abstract Pizza build();
+
+            protected abstract T self();
+        }
+
+        Pizza(Builder<?> builder) {
+            toppings = builder.toppings.clone();
         }
     }
 }
